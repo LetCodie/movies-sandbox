dd79a4a5dfdcd5de34d2e44782f0fec4ce8094da
radin-reth <radin.reth@gmail.com>
2017-07-05 00:22:56 +0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moviesSandboxApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the moviesSandboxApp
+ */
+angular.module('moviesSandboxApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
